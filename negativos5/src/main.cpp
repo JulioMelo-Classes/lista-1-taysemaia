bdd18ceef1c24c5726ec1ba868800fd71a277bd4
Tayse <53482565+taysemaia@users.noreply.github.com>
2021-06-13 16:04:09 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,18 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int i, num, cont_negativos = 0;
+
+    for(i = 0; i < SIZE; i++){
+    	
+    	cin >> num;
+
+    	if(num < 0){
+    		cont_negativos++;
+    	}
+    }
+
+    cout << cont_negativos << std::endl;
 
     return 0;
 }
