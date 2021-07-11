b088e063ff12896ccc5249c5b624dff48ef44ba0
Tayse <53482565+taysemaia@users.noreply.github.com>
2021-07-11 20:31:58 -0300

    Update function.cpp

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,27 @@
 #include "function.h"
+using namespace std;
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
+vector<unsigned int> fib_below_n(unsigned int n)
 {
-    // TODO: adicione o seu código aqui.
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    vector<unsigned int> fibonacci;
+
+    unsigned inicio = 1, inicio2 = 1, soma;
+
+    if (n > 1)
+    {
+        fibonacci.push_back(inicio);
+        fibonacci.push_back(inicio2);
+
+        for (soma = 0; (inicio + inicio2) < n; soma++)
+        {
+            soma = inicio + inicio2;
+            fibonacci.push_back(soma);
+
+            inicio = inicio2;
+            inicio2 = soma;
+        }
+    }
+
+    return vector<unsigned int>{fibonacci};
 }
